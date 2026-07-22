7797152dec4ad2bd1bd6fef2286e0726ae16ab70
Adriyanz1 <adriyan21setiawan@gmail.com>
2026-07-22 22:33:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -113,3 +113,21 @@ function erase() {
 document.addEventListener("DOMContentLoaded", function () {
     if (textArray.length) setTimeout(type, 1000);
 });
+
+// 6. Scroll to Top Button
+const scrollToTopBtn = document.getElementById('scrollToTop');
+
+window.addEventListener('scroll', () => {
+    if (window.pageYOffset > 300) {
+        scrollToTopBtn.classList.add('show');
+    } else {
+        scrollToTopBtn.classList.remove('show');
+    }
+});
+
+scrollToTopBtn.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
